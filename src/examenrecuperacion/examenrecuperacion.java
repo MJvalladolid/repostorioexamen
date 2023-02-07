96d71565ad474bff69879d89e555c7163a1476dd
MJ valladolid <mvalcar1711@g.educaand.es>
2023-02-07 15:47:54 +0100

    comienzo del proyecto

---
+++ src/examenrecuperacion/examenrecuperacion.java
@@ -0,0 +1,15 @@
+package examenrecuperacion;
+
+		import java.io.*;
+		public class examenrecuperacion{
+		public static void main(String[] args) {
+		String comando;
+		System.out.println("Proyecto X");
+		System.out.println("---------------");
+		System.out.println("Seleccione comando de inicio...");
+		System.out.print("opción: ");
+		comando=lectura();
+		ejecutar(comando);
+	}
+
+}
