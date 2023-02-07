4e008ac7a3cf5f024452a2eaab8b9367232f59c0
MJ valladolid <mvalcar1711@g.educaand.es>
2023-02-07 16:14:42 +0100

    calculos.java

--- src/examenrecuperacion/examenrecuperacion.java
+++ src/examenrecuperacion/examenrecuperacion.java
@@ -1,6 +1,18 @@
 package examenrecuperacion;
 
 import java.io.*;
+public class inicioproyecto{
+public static void main(String[] args) {
+String comando;
+System.out.println("Proyecto X");
+System.out.println("---------------");
+System.out.println("Seleccione comando de inicio...");
+System.out.print("opción: ");
+comando=lectura();
+ejecutar(comando);
+}
+}
+import java.io.*;
 import java.util.*;
 public class calculos{
 static void calculo1(){
@@ -10,5 +22,8 @@ System.out.println("Fórmulas cálculo 1");
 static void calculo2(){
 System.out.println("Fórmulas cálculo 2");
 //desarrollo de fórmulas
+}
+static void calculo3() {
+System.out.print("formulas calculo 3");
 }
 }
