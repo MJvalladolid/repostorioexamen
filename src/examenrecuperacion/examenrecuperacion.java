3f7f507ff3253e22f29b9ca0d129d2bfa6133e6d
MJ valladolid <mvalcar1711@g.educaand.es>
2023-02-07 16:11:52 +0100

    lectura.java

--- src/examenrecuperacion/examenrecuperacion.java
+++ src/examenrecuperacion/examenrecuperacion.java
@@ -1,15 +1,14 @@
 package examenrecuperacion;
 
-		import java.io.*;
-		public class examenrecuperacion{
-		public static void main(String[] args) {
-		String comando;
-		System.out.println("Proyecto X");
-		System.out.println("---------------");
-		System.out.println("Seleccione comando de inicio...");
-		System.out.print("opción: ");
-		comando=lectura();
-		ejecutar(comando);
-	}
-
+import java.io.*;
+import java.util.*;
+public class calculos{
+static void calculo1(){
+System.out.println("Fórmulas cálculo 1");
+//desarrollo de fórmulas
+}
+static void calculo2(){
+System.out.println("Fórmulas cálculo 2");
+//desarrollo de fórmulas
+}
 }
